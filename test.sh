29bfbd36ae60ee116b08457c83644aea35d1e5e1
gangl <michael.e.gangl@jpl.nasa.gov>
2019-08-29 11:35:21 -0700

    added testing, fixed invalid json errors

---
+++ test.sh
@@ -0,0 +1,5 @@
+jsonschema -i samples/cumulus_sns_v1.json cumulus_sns_schema.json
+jsonschema -i samples/cumulus_sns_v2.json cumulus_sns_schema.json
+jsonschema -i samples/cumulus_sns_v3.json cumulus_sns_schema.json
+jsonschema -i samples/cumulus_sns_v4.json cumulus_sns_schema.json
+jsonschema -i samples/cumulus_sns_v5.json cumulus_sns_schema.json
