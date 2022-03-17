2e3d47e00d58e831ab97fff3deb9e330bf624ae6
Curtis Banh <30607061+cqbanh@users.noreply.github.com>
2022-03-17 02:21:36 -0700

    Feature/push pypi (#30)
    * Updated release.md
    
    * /version 1.6.0-rc.2 [skip actions]
    
    * /version 1.6.0 [skip actions]
    
    * added build and publish for pypi
    
    * update GHA build - install python wheel
    
    Co-authored-by: nchung <nga.t.chung@jpl.nasa.gov>
    Co-authored-by: podaac-cloud-IandA <podaac-cloud-IandA@jpl.nasa.gov>
    Co-authored-by: Nga Chung <17833879+ngachung@users.noreply.github.com>

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+# with open("README.md", "r", encoding="utf-8") as fh:
+#     long_description = fh.read()
+
+with open('version.txt', "r", encoding="utf-8") as f:
+    version = f.read().strip()
+
+setup(
+    name="podaac-cloud-notification-message-schema",
+    version=version,
+    description="PO.DAAC Cloud Notification Message Schema",
+    url='https://github.com/podaac/cloud-notification-message-schema',
+    author='PO.DAAC',
+    author_email='podaac@podaac.jpl.nasa.gov',
+    install_requires=[
+    ],
+    extras_require={
+        'testing': [
+            'pytest==5.3.5'
+        ]
+    },
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True
+)
